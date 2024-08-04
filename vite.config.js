7a982478320067a7ca4421d877c82d552046be29
Yi-Ping Jiang <apple80177@gmail.com>
2024-08-04 23:50:28 +0800

    feat: header and footer rwd

---
+++ vite.config.js
@@ -0,0 +1,59 @@
+/*
+ * @Author: Yi-Ping Jiang apple80177@gmail.com
+ * @Date: 2024-08-04 16:43:50
+ * @LastEditors: Yi-Ping Jiang apple80177@gmail.com
+ * @LastEditTime: 2024-08-04 17:18:58
+ * @FilePath: /project/glass_website/vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { glob } from 'glob';
+
+import liveReload from 'vite-plugin-live-reload';
+
+function moveOutputPlugin() {
+  return {
+    name: 'move-output',
+    enforce: 'post',
+    apply: 'build',
+    async generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.startsWith('pages/')) {
+          const newFileName = fileName.slice('pages/'.length);
+          bundle[fileName].fileName = newFileName;
+        }
+      }
+    },
+  };
+}
+
+export default defineConfig({
+  // base 的寫法：
+  // base: '/Repository 的名稱/'
+  base: '/glass_website/',
+  plugins: [
+    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+    ViteEjsPlugin(),
+    moveOutputPlugin(),
+  ],
+  server: {
+    // 啟動 server 時預設開啟的頁面
+    open: 'pages/index.html',
+  },
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync('pages/**/*.html')
+          .map((file) => [
+            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+    },
+    outDir: 'dist',
+  },
+});
